8415e3fec6a5d2cf3a74fa5b4930b000d87ef5e9
Rodrigo Vargas <rodrigo.vargas@kairosds.com>
2019-09-30 00:50:51 +0200

    feat(): count function passes all tests

--- counting-bits/PositiveBitCounter.js
+++ counting-bits/PositiveBitCounter.js
@@ -1,5 +1,19 @@
 function Count (input) {
-  throw new Error('Not implemented')
+  if (!Number.isInteger(input) || input < 0) {
+    throw new RangeError('Only unsigned integers allowed')
+  }
+
+  return input
+    .toString(2)
+    .split('')
+    .reverse()
+    .reduce((acc, val, index) => {
+      if (val === '1') {
+        acc[0]++
+        acc.push(index)
+      }
+      return acc
+    }, [0])
 }
 
 module.exports = { Count }
